113648ec086e2092b00c10be3c42153d1ccb03e2
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-01-31 14:31:04 +0000

    Initial commit

---
+++ db/cinema.sql
@@ -0,0 +1,24 @@
+DROP TABLE tickets;
+DROP TABLE films;
+DROP TABLE customers;
+
+
+CREATE TABLE films(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  price INT
+);
+
+CREATE TABLE customers(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT
+);
+
+
+
+CREATE TABLE tickets(
+  id SERIAL PRIMARY KEY,
+  customer_id INT REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT REFERENCES film(id) ON DELETE CASCADE
+);
