18f130022db452c38c3adf622a28dc67a8d60c5f
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-01-31 15:21:16 +0000

    Creation of tables

--- console.rb
+++ console.rb
@@ -6,13 +6,29 @@ require_relative( 'models/ticket')
 
 require( 'pry-byebug' )
 
+customer1 =Customer.new({'name' => 'Mike','funds'=>'100'})
+customer1.save()
+customer2 =Customer.new({'name'=>'David','funds'=>'200'})
+customer2.save()
+customer3=Customer.new({'name'=>'Helen','funds'=>'300'})
+customer3.save()
 
 
+film1=Film.new({'title'=>'Iron Man','price'=>'10'})
+film1.save()
+film3=Film.new({'title'=>'Dragonball','price'=>'20'})
+film3.save()
 
-
-
-
-
+ticket1=Ticket.new({'customer_id'=> customer1.id},'film_id'=>film1.id)
+ticket1.save()
+ticket2=Ticket.new({'customer_id'=> customer3.id, 'film_id'=>film1.id})
+ticket2.save()
+ticket3=Ticket.new({'customer_id'=> customer1.id, 'film_id'=>film2.id})
+ticket3.save()
+ticket4=Ticket.new({'customer_id'=> customer2.id, 'film_id'=>film2.id})
+ticket4.save()
+ticket5=Ticket.new({'customer_id'=> customer3.id, 'film_id'=>film1.id})
+ticket5.save()
 
 binding.pry
 nil
--- db/cinema.sql
+++ db/cinema.sql
@@ -3,22 +3,19 @@ DROP TABLE films;
 DROP TABLE customers;
 
 
-CREATE TABLE films(
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(255),
-  price INT
-);
-
 CREATE TABLE customers(
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
   funds INT
 );
 
-
-
-CREATE TABLE tickets(
+CREATE TABLE films(
   id SERIAL PRIMARY KEY,
-  customer_id INT REFERENCES customers(id) ON DELETE CASCADE,
-  film_id INT REFERENCES film(id) ON DELETE CASCADE
+  title VARCHAR(255),
+  price INT
 );
+
+
+CREATE TABLE tickets(id SERIAL PRIMARY KEY,
+  customer_id INT REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT REFERENCES films(id) ON DELETE CASCADE);
